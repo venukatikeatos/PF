caa1c573878938aff3b4f565a9214462f7cc6fe5
Venu Katike <venukatike02@gmail.com>
2024-03-11 23:10:31 +0530

    added Skills Component

--- src/app/Services/profile-data/profile-data.service.ts
+++ src/app/Services/profile-data/profile-data.service.ts
@@ -17,7 +17,7 @@ export class ProfileDataService {
       livedemo: 'https://personal-portfolio0320.000webhostapp.com/',
       githuburl: 'https://github.com/venukatike/Portfolio',
       imgUrl: '/assets/images/portfolio.png',
-      tech: 'Angular ,Bootstrap'
+      tech: 'Angular , Bootstrap 5, Html    , CSS'
     },
 
     {
@@ -27,7 +27,35 @@ export class ProfileDataService {
       livedemo: 'https://weatherapp-venukatike.netlify.app/',
       githuburl: 'https://github.com/venukatike/JavaScript-Projects',
       imgUrl: "/assets/images/WeatherApp.png",
-      tech: 'Angular, OpenWeatherApi'
+      tech: 'HTML, CSS, JavaScript, OpenWeatherApi'
+    },
+    {
+      id: 3,
+      title: 'Lab Management System',
+      desc: 'LMS is a .NetCore and Angular 16 CRUD Example',
+      livedemo: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
+      githuburl: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
+      mediumlink: 'https://medium.com/analytics-vidhya/spring-boot-angular-8-crud-example-8aeafd47b54',
+      imgUrl: '/assets/images/LMS Project.png',
+      tech: 'Angular 16, DotNet core ,Web Api'
+    }, {
+      id: 1,
+      title: 'My Portfolio',
+      desc: 'My Portfolio developed using Angular and Bootstrap',
+      livedemo: 'https://personal-portfolio0320.000webhostapp.com/',
+      githuburl: 'https://github.com/venukatike/Portfolio',
+      imgUrl: '/assets/images/portfolio.png',
+      tech: 'Angular , Bootstrap 5, Html    , CSS'
+    },
+
+    {
+      id: 2,
+      title: 'WeatherApp',
+      desc: 'Its a weather app developed by me venu katike',
+      livedemo: 'https://weatherapp-venukatike.netlify.app/',
+      githuburl: 'https://github.com/venukatike/JavaScript-Projects',
+      imgUrl: "/assets/images/WeatherApp.png",
+      tech: 'HTML, CSS, JavaScript, OpenWeatherApi'
     },
     {
       id: 3,
@@ -39,52 +67,7 @@ export class ProfileDataService {
       imgUrl: '/assets/images/LMS Project.png',
       tech: 'Angular 16, DotNet core ,Web Api'
     },
-    {
-      id: 10,
-      title: 'DotNet core + Angular 16 CRUD Example',
-      desc: '',
-      livedemo: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      githurl: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      mediumlink: 'https://medium.com/analytics-vidhya/spring-boot-angular-8-crud-example-8aeafd47b54',
-      imgUrl: 's/assets/images/v1.png',
-      tech: 'Angular 16, DotNet core ,Web Api'
-    },{
-      id: 10,
-      title: 'DotNet core + Angular 16 CRUD Example',
-      desc: '',
-      livedemo: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      githurl: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      mediumlink: 'https://medium.com/analytics-vidhya/spring-boot-angular-8-crud-example-8aeafd47b54',
-      imgUrl: 's/assets/images/v1.png',
-      tech: 'Angular 16, DotNet core ,Web Api'
-    },{
-      id: 10,
-      title: 'DotNet core + Angular 16 CRUD Example',
-      desc: '',
-      livedemo: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      githurl: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      mediumlink: 'https://medium.com/analytics-vidhya/spring-boot-angular-8-crud-example-8aeafd47b54',
-      imgUrl: 's/assets/images/v1.png',
-      tech: 'Angular 16, DotNet core ,Web Api'
-    },{
-      id: 10,
-      title: 'DotNet core + Angular 16 CRUD Example',
-      desc: '',
-      livedemo: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      githurl: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      mediumlink: 'https://medium.com/analytics-vidhya/spring-boot-angular-8-crud-example-8aeafd47b54',
-      imgUrl: 's/assets/images/v1.png',
-      tech: 'Angular 16, DotNet core ,Web Api'
-    },{
-      id: 10,
-      title: 'DotNet core + Angular 16 CRUD Example',
-      desc: '',
-      livedemo: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      githurl: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      mediumlink: 'https://medium.com/analytics-vidhya/spring-boot-angular-8-crud-example-8aeafd47b54',
-      imgUrl: 's/assets/images/v1.png',
-      tech: 'Angular 16, DotNet core ,Web Api'
-    },
+    
 
   ]
 
